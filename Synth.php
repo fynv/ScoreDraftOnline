8ecd1264acec6f0b6d125b5c471ed194c12a4e00
fynv <feiy@nvidia.com>
2018-06-24 15:17:17 +0800

    Minor fix

--- Synth.php
+++ Synth.php
@@ -1,5 +1,6 @@
 <?php 
-session_start(); 
+session_start();
+putenv('LANG=en_US.UTF-8'); 
 $name=session_id();
 move_uploaded_file($_FILES["json"]["tmp_name"],"sessions/".$name.".json");
 shell_exec("python ./SynthFromJson.py ".$name);
